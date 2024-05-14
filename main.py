b302cc1932f168619f55933c36dbc105c734624c
Ivan101002 <i.popovich2002@gmail.com>
2024-05-14 16:18:43 +0500

    nice second commiting!

--- main.py
+++ main.py
@@ -10,12 +10,26 @@ def save_strings_to_list(strings_list_on_add):
         if user_input.lower() == 'exit':  # Если пользователь ввел 'exit', выходим из цикла
             break
         # unicode_number = get_unicode_number_first_char(user_input)
-        user_input = split_string(user_input)
-        constant_lower = [is_first_lett_lowercase(user_input[0]), is_first_lett_lowercase(user_input[1]),
-                          is_first_lett_lowercase(user_input[2])]
-        count_true = sum(1 for element in constant_lower if element)
-        if count_true == 3:  # Выбираем этот путь, если все введенные слова начинаются с маленькой буквы
-            strings_list_on_add.append(user_input)  # Добавляем введенную строку в список
+
+        elif user_input.startswith("?-"):  # Если заходим сюда, значит пользователь отправил запрос
+            user_input = split_string(user_input, True)
+            # print(user_input[0], user_input[1], user_input[2], "такой вот запрос")
+            get_requests(user_input, strings_list_on_add)
+
+
+
+
+        else:
+            user_input = split_string(user_input, False)
+            flag_lower = True
+            constant_upper = [is_first_lett_uppercase(user_input[0]), is_first_lett_uppercase(user_input[1]),
+                              is_first_lett_uppercase(user_input[2])]  # Получаем массив True/False для определения
+            # корректности формы ввода
+            if constant_upper[0] or constant_upper[1] or constant_upper[2]:
+                print("Ошибка в команде. Если вы хотите написать запрос, то начните строку с символов ?-")
+                flag_lower = False
+            if flag_lower:
+                strings_list_on_add.append(user_input)  # Добавляем введенную строку в список
 
     return strings_list_on_add  # Возвращаем список сохраненных строк
 
@@ -27,21 +41,51 @@ def get_unicode_number_first_char(
     return unicode_number  # Возвращаем номер символа в таблице Unicode
 
 
-def is_first_lett_lowercase(arr):  # метод для определения, заглавности первого символа
+def get_requests(input_request, data_list):
+    constant_upper = [is_first_lett_uppercase(input_request[0]), is_first_lett_uppercase(input_request[1]),
+                      is_first_lett_uppercase(input_request[2])]  # Получаем массив True/False для определения
+    count_true = constant_upper.count(True)
+    answers = []
+    variable = 0
+    if count_true == 1:  # Заходим в этот блок, при условии, что пользователь ввёл одну переменную
+        variable_index = constant_upper.index(True)  # Весь дальнейший блок отвечает за поиск ответа на запрос
+        # пользователя
+        variable = input_request[variable_index]
+        input_request[variable_index] = None
+        for i, array in enumerate(data_list):
+            matches, answer = 0, 0
+            for j, element in enumerate(array):
+                if element == input_request[j]:
+                    matches += 1
+                else:
+                    answer = element
+                if matches == 2:
+                    answers.append(answer)
+    print(variable, "равна:")
+    for element in answers:
+        print(element)
+    if not answers:
+        print("нет совпадений.")
+
+
+def is_first_lett_uppercase(arr):  # метод для определения, заглавности первого символа
     if len(arr) > 0:
         first_char = arr[0]
-        return first_char.islower()
+        return first_char.isupper()
     else:
         return False
 
 
-def split_string(input_string):
+def split_string(input_string, is_request):
     index1 = input_string.find("(")
     index2 = input_string.find(",")
 
-    part1 = input_string[:index1]  # Часть строки до символа "("
+    if is_request:
+        part1 = input_string[3:index1]  # Часть строки до символа "("
+    else:
+        part1 = input_string[:index1]  # Часть строки до символа "("
     part2 = input_string[index1 + 1:index2]  # Часть строки между символами "(" и ","
-    part3 = input_string[index2 + 2:]  # Часть строки после символа ","
+    part3 = input_string[index2 + 2: len(input_string) - 2]  # Часть строки после символа ","
     result = [part1, part2, part3]
     return result
 
@@ -50,7 +94,7 @@ def show_list(strings_list):
     # Метод для отображения сохранённых строк в базе
     print("Список сохраненных строк:")
     for string in strings_list:
-        print("субъект ", string[1], " делает ", string[0], " с объектом ", string[2])
+        print("субъект", string[1], "делает", string[0], "с объектом", string[2])
     return strings_list
 
 
@@ -63,13 +107,16 @@ def add_list(strings_list):
 
 strings_list = []
 while True:
-    command = input("add - добавить поля \nend - конец работы "
+    command = input("console - работа в консоли \nend - конец работы "
                     "\nprint - записанные объекты \nвведите команду: ")
-    if command.lower() == 'end':
+    command = command.lower()
+    if command == 'end':
         break
-    elif command.lower() == 'add':
+    elif command == 'console':
         strings_list = add_list(strings_list)
-    elif command.lower() == 'print':
+    elif command == 'print':
         strings_list = show_list(strings_list)
+    else:
+        print("Неизвестная команда. Повторите ввод")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
