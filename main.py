e2e3db1ded7b8b36d36b0994c8aa7f52ab5d84db
Ivan101002 <i.popovich2002@gmail.com>
2024-06-14 18:11:53 +0500

    Your code is finally done!

--- main.py
+++ main.py
@@ -2,36 +2,46 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re  # Библиотека для работы с регулярными выражениями
+
 
 # Функция для сохранения строк из пользовательской консоли в список
 def save_strings_to_list(strings_list_on_add):
     while True:
         user_input = input("Введите строку (для выхода введите 'exit'): ")  # Запрашиваем строку у пользователя
+
         if user_input.lower() == 'exit':  # Если пользователь ввел 'exit', выходим из цикла
             break
         # unicode_number = get_unicode_number_first_char(user_input)
 
         elif user_input.startswith("?-"):  # Если заходим сюда, значит пользователь отправил запрос
             user_input = split_string(user_input, True)
-            # print(user_input[0], user_input[1], user_input[2], "такой вот запрос")
-            get_requests(user_input, strings_list_on_add)
+            new_request(user_input, strings_list_on_add)
 
-
-
+        elif ":-" in user_input:
+            final_string = rule_rewriting(user_input)
+            strings_list_on_add.append(final_string)
 
         else:
+            flag_lower = syntax_check(user_input)
             user_input = split_string(user_input, False)
-            flag_lower = True
-            constant_upper = [is_first_lett_uppercase(user_input[0]), is_first_lett_uppercase(user_input[1]),
-                              is_first_lett_uppercase(user_input[2])]  # Получаем массив True/False для определения
-            # корректности формы ввода
-            if constant_upper[0] or constant_upper[1] or constant_upper[2]:
-                print("Ошибка в команде. Если вы хотите написать запрос, то начните строку с символов ?-")
-                flag_lower = False
             if flag_lower:
                 strings_list_on_add.append(user_input)  # Добавляем введенную строку в список
 
     return strings_list_on_add  # Возвращаем список сохраненных строк
+
+
+def syntax_check(input_on_syntax):
+    check_flag = True
+    if not input_on_syntax.endswith(".") != False:
+        print("Ошибка в команде. Команда должна заканчиваться точкой.")
+        check_flag = False
+    input_on_syntax = split_string(input_on_syntax, False)
+    constant_lower = list(map(is_first_lett_lowercase, input_on_syntax))  # Проверка на корректность ввода
+    if any(element == False for element in constant_lower):
+        print("Ошибка в команде. Если вы хотите написать запрос, то начните строку с символов ?-")
+        check_flag = False
+    return check_flag
 
 
 def get_unicode_number_first_char(
@@ -41,81 +51,142 @@ def get_unicode_number_first_char(
     return unicode_number  # Возвращаем номер символа в таблице Unicode
 
 
-def get_requests(input_request, data_list):
-    constant_upper = [is_first_lett_uppercase(input_request[0]), is_first_lett_uppercase(input_request[1]),
-                      is_first_lett_uppercase(input_request[2])]  # Получаем массив True/False для определения
-    count_true = constant_upper.count(True)
-    answers = []
-    variable = 0
-    if count_true == 1:  # Заходим в этот блок, при условии, что пользователь ввёл одну переменную
-        variable_index = constant_upper.index(True)  # Весь дальнейший блок отвечает за поиск ответа на запрос
-        # пользователя
-        variable = input_request[variable_index]
-        input_request[variable_index] = None
-        for i, array in enumerate(data_list):
-            matches, answer = 0, 0
+def new_request(input_request, data_list):
+    constant_lower = list(map(is_first_lett_lowercase, input_request))
+    count_true = constant_lower.count(False)
+    var_index_list = []
+    const_index_list = []
+    summary = []
+    for i, var_index in enumerate(constant_lower):
+        if constant_lower[i]:
+            const_index_list.append([i] + [input_request[i]])
+        else:
+            # var_index_list.append([i] + [input_request[i]])
+            var_index_list.append([input_request[i]])
+        summary.append(input_request[i])
+    indexes = [var_index_list[0]]
+    answers = [[]]
+    term_count = len(var_index_list) + len(const_index_list)
+    for i, array in enumerate(data_list):
+        if term_count == len(array):
+            matches = 0
+            amount = 0
             for j, element in enumerate(array):
-                if element == input_request[j]:
+                if element == summary[j]:
                     matches += 1
-                else:
-                    answer = element
-                if matches == 2:
-                    answers.append(answer)
-    print(variable, "равна:")
-    for element in answers:
-        print(element)
-    if not answers:
-        print("нет совпадений.")
+                elif element != summary[j] and summary[j].isupper():
+                    answers[amount].append(element)
+                    amount += 1
+                    answers.append([])
+    for sub_list in answers[:]:
+        if not sub_list:
+            answers.remove(sub_list)
+    print(answers)
 
 
-def is_first_lett_uppercase(arr):  # метод для определения, заглавности первого символа
+def rule_rewriting(user_list):
+    cleaned_string = re.sub(r'[^\w\s]', ' ', user_list)
+    final_string = re.sub(r'\s+', ' ', cleaned_string)
+    final_string = "Запрос: " + final_string
+    final_string = final_string[:-1].split(" ")
+    return final_string
+
+
+def is_first_lett_lowercase(arr):  # метод для определения, заглавности первого символа
     if len(arr) > 0:
         first_char = arr[0]
-        return first_char.isupper()
+        return first_char.islower()
     else:
         return False
 
 
-def split_string(input_string, is_request):
+def split_string(input_string, user_command):
     index1 = input_string.find("(")
-    index2 = input_string.find(",")
-
-    if is_request:
-        part1 = input_string[3:index1]  # Часть строки до символа "("
+    index3 = input_string.find(")")
+    if user_command:  # Написание новой системы запроса
+        first_word = input_string[3:index1].strip()
     else:
-        part1 = input_string[:index1]  # Часть строки до символа "("
-    part2 = input_string[index1 + 1:index2]  # Часть строки между символами "(" и ","
-    part3 = input_string[index2 + 2: len(input_string) - 2]  # Часть строки после символа ","
-    result = [part1, part2, part3]
+        first_word = input_string[:index1].strip()
+
+    words_inside_brackets = input_string[index1 + 1:index3].split(",")
+    # Обрабатываем случай, когда в скобках только одно слово
+    if len(words_inside_brackets) == 1:
+        words_inside_brackets[0] = words_inside_brackets[0].strip()
+    # Составляем массив
+    result = [first_word] + [word.strip() for word in words_inside_brackets]
     return result
 
 
-def show_list(strings_list):
+def edit_list(list_to_change):
+    # Данный метод необходим для удаления элементов базы знаний
+    if check_on_empty(list_to_change):
+        return list_to_change
+    while True:
+        show_list(list_to_change)
+        index = input("Введите exit для выхода или индекс элемента для удаления:"
+                      "\nвведите all для удаления всей базы знаний: ")
+        if index == "exit":
+            break
+        elif index == "all":
+            list_to_change = []
+            return list_to_change
+        index = int(index)
+        if index < 0 or index >= len(list_to_change):
+            print("Индекс находится за пределами массива")
+        else:
+            list_to_change.pop(index)  # Удаляем элемент по указанному индексу
+    return list_to_change
+
+
+def show_list(lists_to_show):
     # Метод для отображения сохранённых строк в базе
+    if check_on_empty(lists_to_show):
+        return (lists_to_show)
     print("Список сохраненных строк:")
-    for string in strings_list:
-        print("субъект", string[1], "делает", string[0], "с объектом", string[2])
-    return strings_list
+    for i, string in enumerate(lists_to_show):
+        output = [string[0]] + ["(" + word + ")" for word in string[1:]]
+        # print(len(output))
+        if string[0] != "Запрос:":
+            print(i, output)
+            i += 1
+    print("Список сохраненных правил:")
+    for i, string in enumerate(lists_to_show):
+        output = [string[1]] + [word for word in string[2:]]
+        if string[0] == "Запрос:":
+            print(i, output)
+            i += 1
 
 
-def add_list(strings_list):
+def add_list(list_to_save):
     # Вызываем функцию и сохраняем список строк
-    strings_list = save_strings_to_list(strings_list)
-    show_list(strings_list)
-    return strings_list
+    list_to_save = save_strings_to_list(list_to_save)
+    show_list(list_to_save)
+    return list_to_save
+
+
+def check_on_empty(empty_list):  # Метод для проверки того, чтобы пользователь не взаимодействовал с пустым листом
+    if len(empty_list) <= 0:
+        print("В вашей базе знаний ещё нет информации!"
+              "\nФункционал не доступен, пока база знаний пуста.")
+        return True
+    else:
+        return False
 
 
 strings_list = []
 while True:
-    command = input("console - работа в консоли \nend - конец работы "
-                    "\nprint - записанные объекты \nвведите команду: ")
+    command = input("console - работа в консоли \nshow - показать базу знаний "
+                    "\nedit - удалить элемент из базы знаний"
+                    "\nend - конец работы \nвведите команду: ")
     command = command.lower()
     if command == 'end':
         break
     elif command == 'console':
         strings_list = add_list(strings_list)
-    elif command == 'print':
-        strings_list = show_list(strings_list)
+    elif command == 'edit':
+        strings_list = edit_list(strings_list)
+    elif command == 'show':
+        show_list(strings_list)
     else:
         print("Неизвестная команда. Повторите ввод")
 
